9f761c11f6a0756f42a81b41b0072c1b392e22eb
Micah Shute <micah.shute@gmail.com>
2019-05-07 01:48:18 -0500

    Initial Commit

---
+++ intro_oop/functional_dog.rb
@@ -0,0 +1,40 @@
+## Functional Programming
+
+my_dog_1 = {
+    name: "Cricket",
+    breed: "Golden Retriever Mix",
+    temperment: "Lazy",
+    weight: 55,
+    energy: 100,
+    hunger: 100
+}
+
+my_dog_2 = {
+    name: "Griffin",
+    breed: "Carolina Dog",
+    temperment: "Annoying",
+    weight: 65,
+    energy: 100,
+    hunger: 100
+}
+
+def walk_dog(*dogs)
+    dogs.each { |dog| dog[:energy] = 0 }
+end
+
+def feed_dog(dog)
+    dog[:hunger] -= 10
+end
+
+def display_dog_info(dog)
+    puts dog.to_s
+    dog
+end
+
+
+walk_dog(my_dog_1, my_dog_2)
+feed_dog(my_dog_1)
+feed_dog(my_dog_2)
+
+display_dog_info(my_dog_1)
+display_dog_info(my_dog_2)
