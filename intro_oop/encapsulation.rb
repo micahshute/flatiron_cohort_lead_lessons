b9a1de78508d0052f1502f1de55cf679aee4b77b
Micah Shute <micah.shute@gmail.com>
2019-05-14 07:47:50 -0500

    added encapsulation example

---
+++ intro_oop/encapsulation.rb
@@ -0,0 +1,59 @@
+class Dog
+    
+    attr_reader :hunger, :name, :owner
+    
+    def initialize(name, owner = nil)
+        @name = name
+        @owner = owner
+        @hunger = 0
+    end
+
+    def change_name(new_name, owner)
+        @name = new_name if self.owner == owner
+    end
+
+    def owner=(owner)
+        @owner = owner if @owner.nil?
+    end
+
+    def eat
+        @hunger -=30
+        @hunger = 0 if hunger < 0
+    end
+
+end
+
+ 
+class Human
+
+    attr_reader :pets, :name
+
+    def initialize(name)
+        @name = name
+        @pets = []
+    end
+
+    def add_pet(pet)
+        pet.owner = self
+        @pets << pet if pet.owner == self
+    end
+end
+
+roger = Human.new("Roger")
+pongo = Dog.new("Pongo")
+
+pongo.eat 
+puts pongo.hunger # => 0, because our eat method does not allow hunger to drop below 0
+
+pongo.change_name("Pongo you old rascal", roger)
+puts pongo.name # => "Pongo" --> changing the name did not go through because roger is not the owner
+
+roger.add_pet(pongo)
+pongo.change_name("Pongo you old rascal", roger)
+puts pongo.name # => "Pongo you old rascal" because now roger is the owner
+
+
+cruella = Human.new("Cruella DeVile")
+cruella.add_pet(pongo) 
+puts cruella.pets # => an empty array because roger is already owned
+puts pongo.owner.name # => Roger
