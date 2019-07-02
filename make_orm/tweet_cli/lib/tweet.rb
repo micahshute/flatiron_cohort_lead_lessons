f765855d14289e37e6f9a7a6fd38fcec1d6d6669
Micah Shute <micah.shute@gmail.com>
2019-07-01 23:48:36 -0500

    completed example

--- make_orm/tweet_cli/lib/tweet.rb
+++ make_orm/tweet_cli/lib/tweet.rb
@@ -1,48 +1,99 @@
 class Tweet
 
+    def self.count
+        sql = <<-SQL
+            SELECT count(*) FROM tweets;
+        SQL
+        data = DB[:conn].execute(sql)
+        return data.first.values.first
+    end
 
     def self.all
         sql = <<-SQL
-            SELECT * FROM tweets
+            SELECT * FROM tweets;
         SQL
-        # binding.pry
-        data = DB[:conn].execute(sql)
-        
-        data.map do |tweet_data|
-            xformed_tweet = self.map_data(tweet_data)
-            Tweet.new(xformed_tweet)
-        end
+
+        return self.map_from_db(DB[:conn].execute(sql))
     end
 
-    def self.map_data(data)
-        return data.each_with_object({}){ |(k,v), memo| memo[k.to_sym] = v}
+    def self.delete_by_id(id)
+        sql = <<-SQL
+            DELETE FROM tweets WHERE id = ?;
+        SQL
+        DB[:conn].execute(sql, id)
     end
 
     def self.find_by_id(id)
         sql = <<-SQL
-            SELECT * FROM tweets WHERE id = ?
+            SELECT * FROM tweets WHERE id = ?;
         SQL
-        data = DB[:conn].execute(sql, id)
-        self.new(map_data(data[0]))
-    end
+
+        return self.map_from_db(DB[:conn].execute(sql, id).first)
+    end 
     
-    attr_accessor :username, :message
-    attr_reader :id
-    def initialize(username:, message:, id: nil)
-        @username, @message = username, message
-        @id = id
+    def self.find_by_username(username)
+        # NOTE: This lookup is CASE INSENSITIVE
+        sql = <<-SQL
+            SELECT * FROM tweets
+            WHERE lower(username) = ?;
+        SQL
+        return self.map_from_db(DB[:conn].execute(sql, username.downcase))
     end
 
+    def self.find_by_content(content)
+        sql = <<-SQL
+            SELECT * FROM tweets
+            WHERE lower(message) like ?
+        SQL
+        return self.map_from_db(DB[:conn].execute(sql, "%#{content.downcase}%"))
+    end
+
+    def self.map_from_db(data)
+        if data.is_a?(Array)
+            return data.map{ |d| self.new_from_db(d) }
+        elsif data.is_a?(Hash)
+            return self.new_from_db(data)
+        else
+            raise ArgumentError.new("Unexpected datatype")
+        end
+    end 
+
+    def self.new_from_db(data)
+        self.new(self.xform_data(data))
+    end
+
+    def self.xform_data(data)
+        return data.each_with_object({}){ |(k,v), memo| memo[k.to_sym] = v }
+    end
+
+    attr_accessor :username, :message
+    attr_reader :id # <-- Encapsulation
+    def initialize(username: , message: , id: nil)
+        @username, @message, @id = username, message, id
+    end
 
     def save
-        sql  = <<-SQL
-            INSERT INTO tweets (username, message)
-            VALUES (?, ?)
-        SQL
+        if self.saved?
+            sql = <<-SQL
+                UPDATE tweets
+                SET username = ?, message = ?
+                WHERE id = ?;
+            SQL
+            DB[:conn].execute(sql, self.username, self.message, self.id)
+        else
+            sql = <<-SQL
+                INSERT INTO tweets (username, message)
+                VALUES (?, ?);
+            SQL
+            
+            DB[:conn].execute(sql, self.username, self.message)
+            @id = DB[:conn].last_insert_row_id
+        end
 
-        data = DB[:conn].execute(sql, self.message, self.username)
-        @id = DB[:conn].last_insert_row_id
     end
 
+    def saved?
+        !self.id.nil?
+    end
 
 end
--- make_orm/tweet_cli/lib/tweet_app.rb
+++ make_orm/tweet_cli/lib/tweet_app.rb
@@ -1,28 +1,94 @@
-class TweetApp
+
+class TweetsApp
 
 
     def start
-        while true
+        loop do
+            self.display_menu_options
+            thing_to_do = gets.strip
+            case thing_to_do
+            when "id"
+                puts "Please enter an id to search for"
+                id = self.validate_tweet_id
+                next if id.nil?
+                tweet = Tweet.find_by_id(id)
+                puts; puts;
+                display_tweet(tweet)
+            when "n"
+                puts "Enter a username:"
+                username = gets.strip
+                puts "Enter a message:"
+                message = gets.strip
+                tweet = Tweet.new(username: username, message: message)
+                tweet.save
+                display_tweet(tweet)
+            when "d"
+                puts "Enter the id of the tweet you wish to delete"
+                id = self.validate_tweet_id
+                next if id.nil?
+                Tweet.delete_by_id(id)
+                puts "\n\nRecord expunged\n\n"
+            when "su"
+                puts "Enter the username of the tweets you wish to lookup"
+                username = gets.strip.downcase
+                self.display_tweets(Tweet.find_by_username(username))
+            when "sc"
+                puts "Enter the content you wish to search for"
+                content = gets.strip.downcase
+                self.display_tweets(Tweet.find_by_content(content))
+            when "ls"
+                render_tweets
+            when "e"
+                puts "Goodbye."
+                return
+            else
+                puts "That is not a supported function"
+            end
+            
+           
 
-            puts "Enter a username:"
-            username = gets.strip
-            puts "Enter a message:"
-            message = gets.strip
-
-            tweet = Tweet.new(username: username, message: message)
-            tweet.save
-            render_tweets
-            binding.pry
         end
     end
 
+    def display_menu_options
+        puts
+        puts "What do you want to do?"
+        puts "-----------------------"
+        puts "[id] Tweet lookup by id"
+        puts "[n] Make new tweet"
+        puts "[d] Delete tweet by id"
+        puts "[su] Search tweets by username"
+        puts "[sc] Search tweets by content"
+        puts "[ls] Display all tweets"
+        puts "[e] To exit program"
+        puts
+    end
+
+    def validate_tweet_id
+        id = gets.strip.to_i
+        while id <= 0 || id > Tweet.count
+            puts "You must enter a number between 1 and #{Tweet.count}. Type q to return to main menu."
+            id = gets.strip
+            return nil if id.downcase == "q"
+            id = id.to_i
+        end
+        return id
+    end
+
+    def display_tweets(tweets)
+        tweets.each{ |t| self.display_tweet(t)}
+    end
+
+    def display_tweet(tweet)
+        puts "\n----------------------------------------------------"
+        puts "| #{tweet.id}. #{tweet.username}\n| \n| #{tweet.message}\n"
+        puts "----------------------------------------------------\n\n"
+    end
 
     def render_tweets
-
-        Tweet.all.each.with_index(1) do |tw, i|
-            puts "#{i}. #{tw.username} \n #{tw.message}\n\n\n"
+        Tweet.all.each do |tweet|
+            display_tweet(tweet)
         end
-
     end
 
 end
