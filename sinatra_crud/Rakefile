f18749a2dc02e83ac2fb97b7618af43a8a397102
Micah Shute <micah.shute@gmail.com>
2019-07-29 15:45:45 -0500

    begin sinatra crud lesson

---
+++ sinatra_crud/Rakefile
@@ -0,0 +1,8 @@
+require './config/env'  
+require 'sinatra/activerecord/rake'
+
+
+desc "Mess with models in console" 
+task :console do
+    Pry.start
+end
---
+++ sinatra_crud/config/env.rb
@@ -0,0 +1,11 @@
+ENV['SINATRA_ENV'] ||= 'development'
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+
+configure :development do
+    set :database, 'sqlite3:db/database.db'
+end
+
+require_all 'app'
