55281bd06cf32934f396a8d1aa330ba5e11ad39c
Micah Shute <micah.shute@gmail.com>
2019-07-29 16:34:49 -0500

    add application controller class

--- sinatra_crud/app/controllers/application_controller.rb
+++ sinatra_crud/app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < Sinatra::Base
 
 
+    configure do
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, ENV['SESSION_SECRET']
+    end
+
+
 end
