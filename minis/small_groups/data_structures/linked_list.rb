2b93d740050fd9f58e050eefbbc6fea322603eb3
Micah Shute <micah.shute@gmail.com>
2019-09-15 23:31:25 -0700

    added linked list lesson

--- minis/small_groups/data_structures/linked_list.rb
+++ minis/small_groups/data_structures/linked_list.rb
@@ -1,17 +1,88 @@
+require 'pry'
+
 class Node
 
-    attr_accessor :next
+    attr_accessor :next, :data
 
     def initialize(data)
         @data, @next = data, nil
+    end
+
+    def insert_after_self(node)
+        node.next = self.next 
+        self.next = node
+    end
+
+    def delete_next_node
+        self.next = self.next.next
     end
 
 end
 
 class LinkedList
 
-    def initialize(head)
+    def self.create_from_array(arr)
+        list = LinkedList.new
+        for i in (arr.length - 1).downto(0)
+            list.insert(node: Node.new(arr[i]), index: 0)
+        end
+        list
+    end
+
+    def initialize(head = nil)
         @head = head
     end
 
+    def node_at(index)
+        node = @head
+        index.times{ node = node.next }
+        node
+    end
+
+    def insert(node: , index: )
+        if index == 0
+            node.next = @head
+            @head = node
+        else
+            curr_node = @head
+            (index - 1).times{ curr_node = curr_node.next }
+            curr_node.insert_after_self(node)
+        end
+        
+    end
+
+    def delete(index)
+        if index == 0
+            @head = @head.next 
+        else
+            curr_node = @head
+            (index - 1).times{ curr_node = curr_node.next }
+            curr_node.delete_next_node
+        end
+    end
+
+    def print_list
+        node = @head
+        print "#{node.data}"
+        node = node.next
+        while node
+            print " -> #{node.data}"
+            node = node.next
+        end
+        puts
+    end
+
 end
+
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+list = LinkedList.create_from_array(arr)
+list.print_list
+
+list.insert(node: Node.new(11), index: 10)
+list.insert(node: Node.new(3.5), index: 3)
+list.print_list
+list.delete(3)
+list.print_list
+
+binding.pry
---
+++ minis/small_groups/data_structures/linked_list_lesson.md
@@ -0,0 +1,1 @@
+[Linked List Slides](https://docs.google.com/presentation/d/1looml4CpvGL8WgL30-Hkkl3uirTxBzl9fW6GF1OhzXc/edit?usp=sharing)
